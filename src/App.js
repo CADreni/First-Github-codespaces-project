e8f0f6593a8419a0040b5b4ea86b7c20862f6e94
CADreni <118628726+CADreni@users.noreply.github.com>
2022-12-22 14:22:14 +0000

    cloned repo

--- src/App.js
+++ src/App.js
@@ -6,21 +6,9 @@ function App() {
       <header className="App-header">
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
-          Hello world
+          This is our cloned GitHub Codespaces Repository
         </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+        
       </header>
     </div>
   );
